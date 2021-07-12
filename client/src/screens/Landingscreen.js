808e5e10fe567375c5ccef232e8b5fe1e37ad6e4
Prasanth Sanjeevi <prasanths@sssbi.com>
2021-07-12 15:35:29 +0530

    first commit

--- client/src/screens/Landingscreen.js
+++ client/src/screens/Landingscreen.js
@@ -1,27 +1,30 @@
 import React from "react";
-import moment from "moment"
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import moment from "moment";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
 AOS.init({
-    duration:'2000'
+  duration: "2000",
 });
 function Landingscreen() {
   return (
     <div className="">
       <div className="landing row justify-content-center text-center">
-        <div className="col-md-9 my-auto" style={{borderRight:'8px solid white'}}>
-          <h2 style={{ color: "white", fontSize: "130px" }} data-aos='zoom-in'>SheyRooms</h2>
-          <h1 style={{ color: "white"}} data-aos='zoom-out' >“There is only one boss. The Guest.</h1>
+        <div
+          className="col-md-6 my-auto"
+          style={{ borderRight: "6px solid white" }}
+        >
+          <h2 style={{ color: "white", fontSize: "80px" }} data-aos="zoom-in">
+            Jayaram Residency
+          </h2>
+          <h1 style={{ color: "white" }} data-aos="zoom-out">
+            “Tirupathi Chittoor and Srikalahasthi
+          </h1>
           <Link to="/home">
-             <button className='btn btn-primary'>Get Started</button>
+            <button className="btn btn-primary">Book Now</button>
           </Link>
         </div>
-
-        
-        
       </div>
-     
     </div>
   );
 }
